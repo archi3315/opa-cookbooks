88a9941c8a4041e6a8491538ae2a0e54da817dc3
archi <aarchimowicz@gmail.com>
2014-10-05 00:13:46 -0300

    Add dependency with aws

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ maintainer_email "admin@opadescuentos.com"
 description      "Installs and configures opa modules"
 version          "1.0.0"
 
+depends "aws"
+
 recommends 'java'
 recommends 'monit'
 
